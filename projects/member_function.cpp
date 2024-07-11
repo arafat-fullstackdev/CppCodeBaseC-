e631830c5d1dbe714cc95d59534929b697135085
Arafat Patwary <farabipsk@gmail.com>
2024-07-11 23:25:07 +0600

    remove clrscr class

--- projects/member_function.cpp
+++ projects/member_function.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
+#include <conio.h> // Added for clrscr() and getch()
 using namespace std;
+
 class student 
 {
     char name[20];
-    int rn,s1,s2,s3;
+    int rn,s1,s2,s3,total;
     float prc;
 
     public:
           void getData()
           {
-            cout<<"Enter Your name:";
+            cout<<"n/Enter Your name:";
             cin>>name;
             cout<<"Enter Your Roll No:";
             cin>>rn;
@@ -19,20 +21,22 @@ class student
           void calculate()
           {
             total = s1+s2+s3;
-            prc = total/3;
+            prc = static_cast<float>(total) / 3; //Ensure proper division
           }
           void display()
           {
-            cout<<"Name"<<name;
-            cout<<"Roll"<<rn;
-            cout<<"total"<<total;
-            cout<<"Perchantage"<<prc;
+            cout<<"Name:"<<name<<endl;
+            cout<<"Roll:"<<rn<<endl;
+            cout<<"total:"<<total<<endl;;
+            cout<<"Percentage:"<<prc<<"%"<<endl;
           }
 };
 
-void main(){
+int main(){
+
     student s;
-    clrscr();
+    //clrscr();
+    
     s.getData();
     s.calculate();
     s.display();
